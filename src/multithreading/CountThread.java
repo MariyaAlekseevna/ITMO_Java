cd7ceb20332e2b66955867ea915357b91a77c753
MariyaAlekseevna <maria.009@mail.ru>
2023-03-29 20:51:42 +0300

    add multithreading

---
+++ src/multithreading/CountThread.java
@@ -0,0 +1,17 @@
+package multithreading;
+
+public class CountThread extends Thread {
+    private Counter counter;
+
+    public CountThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+
+}
---
+++ src/multithreading/Counter.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+public class Counter {
+    int count = 0;
+
+    public void increment() {
+        count = count + 1;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
