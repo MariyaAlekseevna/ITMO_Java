50431acc9be013c18a86fdd47ebfd068bdf13a74
MariyaAlekseevna <maria.009@mail.ru>
2023-03-29 21:36:33 +0300

    add multithreading class Main

---
+++ src/multithreading/Main.java
@@ -0,0 +1,38 @@
+package multithreading;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("==================  Задание 1 ==================");
+        for (int i = 1; i <= 10; i++) {
+            new ThreadHundred().start();
+        }
+
+        System.out.println("==================  Задание 2 ==================");
+        ThreadState threadState = new ThreadState();
+        System.out.println("Состояние потока перед его запуском: " + threadState.getState());
+        threadState.start();
+        System.out.println("Состояние потока после его запуска: " + threadState.getState());
+
+        System.out.println("==================  Задание 3 ==================");
+        Counter counter = new Counter();
+        for (int i = 1; i <= 100; i++) {
+            CountThread countThread = new CountThread(counter);
+            countThread.start();
+            try {
+                countThread.join();
+                System.out.println("Counter " + i + " = " + +counter.getCount());
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println("==================  Задание 4 ==================");
+        Object object = new Object();
+        ThreadName threadName1 = new ThreadName(object);
+        ThreadName threadName2 = new ThreadName(object);
+        threadName1.start();
+        threadName2.start();
+
+    }
+}
